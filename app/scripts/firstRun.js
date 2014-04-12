d719681849bf3cf6712f68f43760d5ed82acda4d
Conner Swann <me@connerswann.me>
2014-04-12 09:04:15 -0700

    worked on firstRun.js, I think I fixed it

--- app/scripts/firstRun.js
+++ app/scripts/firstRun.js
@@ -1,15 +1,34 @@
 /**
  * Created by ryanbatchelder on 4/12/14.
  */
-var spritzText = "You've successfully installed Spritz for Chrome!";
-var onFetchSucessController = function(spritzText) {
-    startSpritzing(spritzText);
+
+function init(){
+	//initialize the controller from the API
+	spritzController = new SPRITZ.spritzinc.SpritzerController(customOptions);
+	//attach the controller div
+	spritzController.attach($"#spritzer");
 }
 
-function startSpritzing(spritzText) {
-    $("#spritzer").data("controller").startSpritzing(spritzText);
+function startSpritzing(text) {
+	var locale = "en_us";
+	spritzClient.spritzify(text, locale, onSpritzifySuccess, onSpritzifyFailure)
+	
 }
+
+function onSpritzifySuccess(){
+	console.log("Spritz fetched the text!");
+}
+
+function onSpritzifyFailure(){
+	console.log("Spritz failed... Sorry about that.");
+}
+
 
 $(document).ready(function() {
-    SpritzClient.fetchContents(info.url, onFetchSuccessController, onFetchError);
+	init();
+	
+	//feed the text to spritzify
+	var spritzText = "You've successfully installed Spritz for Chrome!";
+	startSpritzing(spritzText);
+	
 })
