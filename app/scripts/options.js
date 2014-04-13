328220dc39645e21d12bae48e502ef3238dabae3
Ryan Batchelder <rb688@nau.edu>
2014-04-13 00:40:07 -0700

    Working on options page, almost done, working out some issues with offline storage.

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,27 @@
-'use strict';
+function save_options() {
+    var wpm = document.getElementById('wpm').value;
+    chrome.storage.sync.set({
+        'WordsPerMin': wpm
+    }, function() {
+        var status = document.getElementById('status');
+        status.textContent = 'Options Saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 2000);
+    });
+}
 
-console.log('\'Allo \'Allo! Option');
+function restore_options() {
+    chrome.storage.sync.get({
+        WordsPerMin: wpm
+    }, function(items) {
+        document.getElementById('wpm').value = items.WordsPerMin;
+    })
+}
+
+window.onload = function() {
+    document.addEventListener('DOMContentLoaded', restore_options);
+    document.getElementById('save').addEventListener('click',
+        save_options);
+};
+
