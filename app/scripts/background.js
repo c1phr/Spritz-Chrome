e562cfc4aa5a309a95b8629e018c82ebe9dbe0da
Ryan Batchelder <rb688@nau.edu>
2014-04-12 18:05:33 -0700

    Broken extension to page interaction

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,16 +1,21 @@
 'use strict';
 
+var spritzerController = null;
 
 $(document).ready(function() {
     var myuri = chrome.extension.getURL("/login_success.html")
+
     var SpritzSettings = {
         clientId: "4739145546ba944ad",
         redirectUri: myuri
     };
+    var customOptions = {
+        "redicleWidth" : 	434,	// Specify Redicle width
+        "redicleHeight" : 	76		// Specify Redicle height
+    };
     $.getScript("https://sdk.spritzinc.com/js/1.0/js/spritz.min.js", function () {
         console.log("DocumentReady got SDK file");
         window.SpritzClient = new SPRITZ.client.SpritzClient(SpritzSettings.clientId, 'https://api.spritzinc.com/api-server/v1/', myuri);
-
 		window.spritz_sdk_root = "https://sdk.spritzinc.com/js/1.0";
 		//var head  = document.getElementsByTagName('head')[0];
 	    //var link  = document.createElement('link');
@@ -20,6 +25,10 @@ $(document).ready(function() {
 	    window.link.href = window.spritz_sdk_root + '/css/spritz.min.css';
 	    //link.media = 'all';
 	    window.head.appendChild(window.link);
+        spritzerController = new SPRITZ.spritzinc.SpritzerController(customOptions);
+        //spritzerController.spritzClient = SpritzClient;
+
+
     });
 	//$.getScript("https://sdk.spritzinc.com/js/1.0/css/spritz.min.css", function(){console.log("got the Spritz CSS")});
 });
@@ -74,7 +83,7 @@ chrome.contextMenus.onClicked.addListener(function(data) {
 function startSpritzing(text) {
     var locale = "en_us";
     var successHandler = function(text){
-        //spritzController.setSpritzText(text);
+        spritzerController.startSpritzing(text);
         console.log("Spritz fetched the text!");
     };
     var failureHandler = function(text){
@@ -96,7 +105,9 @@ function getSelectionText() {
 
 function displayRedicle() {
     console.log("Redicle Display Called");
+    $("body").prepend("<div class='backdrop overlay'>");
+    $("body").append("</div><div class='reader-wrapper'><div data-role='spritzer' id='spritzer'></div></div>");
+    console.log($("body").html);
+    spritzerController.attach($('#spritzer'));
     getSelectionText();
-    $("html").prepend("<div class='backdrop overlay'>");
-    $("html").append("</div><div class='reader-wrapper'><div data-role='spritzer' id='spritzer'></div></div>");
 }
